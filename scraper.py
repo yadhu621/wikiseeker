1e400c32ae8a98b968ec7baed26fe24ef888ba56
Yadhu <yadhu621@gmail.com>
2023-11-14 15:24:31 +0000

    rename to html_downloads

--- scraper.py
+++ scraper.py
@@ -8,21 +8,21 @@ class WikiSpider(scrapy.Spider):
         'https://quotes.toscrape.com/page/1/',
         'https://quotes.toscrape.com/page/2/',
         'https://quotes.toscrape.com/page/3/',
-        'https://quotes.toscrape.com/page/4/',
-        'https://quotes.toscrape.com/page/5/',
-        'https://quotes.toscrape.com/page/6/',
-        'https://quotes.toscrape.com/page/7/',
-        'https://quotes.toscrape.com/page/8/',
-        'https://quotes.toscrape.com/page/9/',
-        'https://quotes.toscrape.com/page/10/',
+        # 'https://quotes.toscrape.com/page/4/',
+        # 'https://quotes.toscrape.com/page/5/',
+        # 'https://quotes.toscrape.com/page/6/',
+        # 'https://quotes.toscrape.com/page/7/',
+        # 'https://quotes.toscrape.com/page/8/',
+        # 'https://quotes.toscrape.com/page/9/',
+        # 'https://quotes.toscrape.com/page/10/',
         ]
 
     def parse(self, response):
-        # Ensure the 'html' folder exists
-        os.makedirs('html', exist_ok=True)
+        # Ensure the 'html_downloads' folder exists
+        os.makedirs('html_downloads', exist_ok=True)
 
-        # Specify the path including the 'html' folder
-        filename = os.path.join('html', 'quotes.html')
+        # Specify the path including the 'html_downloads' folder
+        filename = os.path.join('html_downloads', 'quotes.html')
 
         # Open the file in append mode and write the response body
         with open(filename, 'ab') as f:
